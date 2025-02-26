ed40b9083c8060b523f1ea2c22c3984ff2454c67
nmthien1810 <nmthien1810@gmail.com>
2025-02-26 20:56:14 +0700

    .

---
+++ bai1.cpp
@@ -0,0 +1,17 @@
+#include "D:\LTDT\CT175\matrandinhcung.cpp"
+
+int main() {
+    Graph G;
+    int n = 4, m = 5;
+    init_graph(&G, n, m);
+
+    add_edge(&G, 1, 1, 2); 
+    add_edge(&G, 2, 1, 3); 
+    add_edge(&G, 3, 1, 3); 
+    add_edge(&G, 4, 3, 4); 
+    add_edge(&G, 5, 1, 4); 
+
+    for (int v = 1; v <= n; v++) {
+        printf("deg(%d) = %d\n", v, degree(&G, v));
+    }
+}
