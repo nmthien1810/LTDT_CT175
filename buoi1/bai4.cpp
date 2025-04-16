1d0aef4a75d3fa7eb49be4a77ae40cde0507f427
nmthien1810 <nmthien1810@gmail.com>
2025-04-16 14:10:26 +0700

    .

--- buoi1/bai4.cpp
+++ buoi1/bai4.cpp
@@ -5,11 +5,11 @@ int main() {
     Graph G; 
     int n = 4; 
     init_graph(&G, n); 
-    add_edge(&G, 1, 2); 
-    add_edge(&G, 1, 3); 
-    add_edge(&G, 1, 3); 
-    add_edge(&G, 3, 4); 
-    add_edge(&G, 1, 4); 
+    add_edge1(&G, 1, 2); 
+    add_edge1(&G, 1, 3); 
+    add_edge1(&G, 1, 3); 
+    add_edge1(&G, 3, 4); 
+    add_edge1(&G, 1, 1); 
 
     for (int v = 1; v <= n; v++) 
         printf("deg(%d) = %d\n", v, degree(&G, v)); 
